a2700b393c94aa34051c7ea3a8a5ba2808628e64
Ali fani <alifani1377@gmail.com>
2023-10-23 23:30:58 +0330

    EP02

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from settings import *
 from sys import exit
+
+from game import Game
+from score import Score
+from preview import Preview
+
 class Main:
     def __init__(self) -> None:
         
@@ -7,6 +12,11 @@ class Main:
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
         self.clock = pygame.time.Clock()
         pygame.display.set_caption("Tetris")
+
+        # components
+        self.game = Game()
+        self.score = Score()
+        self.preview = Preview()
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -15,6 +25,11 @@ class Main:
                     exit()
             # display
             self.display_surface.fill(GRAY)
+
+            # compoenents
+            self.game.run()
+            self.score.run()
+            self.preview.run()
 
             # updating the game
             pygame.display.update()
