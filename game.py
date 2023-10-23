3fb93e11df18442425b20794b4567b6e9b586cb8
Ali fani <alifani1377@gmail.com>
2023-10-24 00:06:19 +0330

    EP04

--- game.py
+++ game.py
@@ -1,16 +1,20 @@
 from settings import *
-
+from random import choice
 class Game:
     def __init__(self) -> None:
         
         self.surface = pygame.Surface((GAME_WIDTH,GAME_HEIGHT))
         self.display_surface = pygame.display.get_surface()
         self.rect = self.surface.get_rect(topleft = (PADDING,PADDING))
+
+        self.sprites = pygame.sprite.Group()
+
         self.grid_surface = self.surface.copy()
         self.grid_surface.fill((0,255,0))
         self.grid_surface.set_colorkey((0,255,0))
         self.grid_surface.set_alpha(120)
 
+        self.tetromino = Tetromino(choice(list(TETROMINOS.keys())),self.sprites)
 
     def draw_grid(self):
         for col in range(1, COLUMNS):
@@ -23,8 +27,29 @@ class Game:
     def run(self):
 
         self.surface.fill(GRAY)
-
+        self.sprites.draw(self.surface)
 
         self.draw_grid()
         self.display_surface.blit(self.surface, (PADDING,PADDING))
         pygame.draw.rect(self.display_surface,LINE_COLOR,self.rect,2,2)
+
+class Tetromino:
+    def __init__(self,shape,group) -> None:
+        
+        self.block_positions = TETROMINOS[shape]['shape']
+        self.color = TETROMINOS[shape]['color']
+
+        self.blocks = [Block(group, pos, self.color) for pos in self.block_positions]
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, group, pos, color) -> None:
+        # general
+        super().__init__(group)
+        self.image = pygame.Surface((CELL_SIZE,CELL_SIZE))
+        self.image.fill(color)
+
+        # position
+        self.pos = pygame.Vector2(pos) + BLOCK_OFFSET
+        x = self.pos.x * CELL_SIZE
+        y = self.pos.y * CELL_SIZE
+        self.rect = self.image.get_rect(topleft = (x,y))
